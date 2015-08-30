4c586e0a4a29aca19ff2a868215377fcb0a365bc
y_okada <yasokada@gmail.com>
2015-08-31 06:36:54 +0900

    can switch between Main and Setting by buttons

---
+++ Assets/SettingButtonControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingButtonControl : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void onClick() {
+		Application.LoadLevel ("Setting");
+	}
+}
