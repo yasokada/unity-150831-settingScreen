17c059d760da308da16caa7cdc68b33df00f4604
y_okada <yasokada@gmail.com>
2015-08-31 07:02:39 +0900

    add requester and responder

---
+++ Assets/RequesterScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RequesterScript : MonoBehaviour {
+
+	public Text text_ipadr;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		text_ipadr.text = ResponderScript.str_ipadr;
+	}
+}
--- Assets/ResponderScript.cs
+++ Assets/ResponderScript.cs
@@ -1,13 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class ResponderScript : MonoBehaviour {
 
+	public InputField IF_ipadr;
+	public static string str_ipadr = "";
+
 	void Start () {
 		DontDestroyOnLoad (this); // to access public static variables from other scene	
+		loadOldSetting ();
 	}
-	
-	void Update () {
-	
+
+	void loadOldSetting() {
+		IF_ipadr.text = str_ipadr;
+	}
+
+	void OnGUI() {
+		if (str_ipadr.Equals (IF_ipadr.text) == false) {
+			str_ipadr = IF_ipadr.text;
+		}
 	}
 }
