656511c85d70a79e233a34bee54bda085b828d34
y_okada <yasokada@gmail.com>
2015-08-31 07:06:20 +0900

    add version comment

--- Assets/RequesterScript.cs
+++ Assets/RequesterScript.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 using UnityEngine.UI;
 
+
+/*
+ * v0.1 2015/08/31
+ *   - set and read Input Field
+ */
+
 public class RequesterScript : MonoBehaviour {
 
 	public Text text_ipadr;
